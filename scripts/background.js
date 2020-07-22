a6011f36de2b6b1818c17c0f03b59b0dd4f5ef60
Jonas <jonas-gruempel@gmx.de>
2020-07-22 09:37:53 +0200

    Update background.js

--- scripts/background.js
+++ scripts/background.js
@@ -1,11 +1,11 @@
 chrome.runtime.onInstalled.addListener(function() {
     chrome.runtime.onMessage.addListener(function(message, callback) {
-        if (message == "runContentScript"){
+        if (message === 'invadeBrowser'){
             chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-                chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
+                chrome.tabs.sendMessage(tabs[0].id, 'answer', function(response) {
                     callback();
                 });
-            });      
+            });
         }
     });
 });
